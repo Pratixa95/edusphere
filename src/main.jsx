fe4374f59c1b3c9a85e26b61841601cafc0e6063
Pratixa95 <mahetapratixa7695@gmail.com>
2026-01-21 15:52:29 +0530

    toggler change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+
+// ✅ Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+// ✅ Bootstrap JS (THIS FIXES TOGGLER)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
