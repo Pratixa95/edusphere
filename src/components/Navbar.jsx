376e0eaa6aefab9b8270d74021704063672aaf4d
Pratixa95 <mahetapratixa7695@gmail.com>
2026-01-21 15:38:07 +0530

    improved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,56 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm fixed-top">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top navbar-custom">
       <div className="container">
-        
-        <Link className="navbar-brand fw-bold" to="/">
-          EduSphere
+
+        {/* ===== LOGO ===== */}
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img
+            src={logo}
+            alt="EduSphere Logo"
+            className="navbar-logo"
+          />
+          <span className="fw-bold ms-2">EduSphere</span>
         </Link>
 
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/courses">Courses</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
-        </ul>
+        {/* ===== MOBILE TOGGLER ===== */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#edusphereNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* ===== NAV LINKS ===== */}
+        <div className="collapse navbar-collapse" id="edusphereNavbar">
+          <ul className="navbar-nav ms-auto align-items-lg-center">
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/courses">Courses</NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+
+            <li className="nav-item ms-lg-3">
+              <NavLink className="btn btn-primary px-4" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+
+          </ul>
+        </div>
 
       </div>
     </nav>
